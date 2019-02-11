83f3dbcd17b48586308e2e4e9197d391be95c094
abbyg1205 <abby.goswami@gmail.com>
2019-02-11 11:40:28 +0100

    user not found

---
+++ quora-api/src/main/java/com/upgrad/quora/api/exception/UserNotFoundExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.upgrad.quora.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import com.upgrad.quora.api.Response;
+import com.upgrad.quora.service.exception.*;
+
+@ControllerAdvice
+@RestController
+public class UserNotFoundExceptionHandler {
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	  public final ResponseEntity<Response> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+	    Response response = new Response(ex.getCode(),ex.getErrorMessage());
+	    return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+	}
+}
