8f1d43c0e4d70e26e5a0bdd9e0e55659b643d0d5
abbyg1205 <abby.goswami@gmail.com>
2019-02-11 11:38:05 +0100

    update context

--- quora-api/src/main/java/com/upgrad/quora/api/QuoraApiApplication.java
+++ quora-api/src/main/java/com/upgrad/quora/api/QuoraApiApplication.java
@@ -3,7 +3,11 @@ package com.upgrad.quora.api;
 import com.upgrad.quora.service.ServiceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * A Configuration class that can declare one or more @Bean methods and trigger auto-configuration and component scanning.
@@ -11,6 +15,11 @@ import org.springframework.context.annotation.Import;
  */
 @SpringBootApplication
 @Import(ServiceConfiguration.class)
+@Configuration
+@EnableJpaRepositories(basePackages = {"com.upgrad.quora.service.dao"})
+@ComponentScan("com.upgrad.quora.service")
+@ComponentScan("com.upgrad.quora.api.controller")
+@EntityScan("com.upgrad.quora.service.entity")
 public class QuoraApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuoraApiApplication.class, args);
